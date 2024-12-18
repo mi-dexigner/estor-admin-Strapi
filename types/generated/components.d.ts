36126fe0aa10b161e784d77a76b8735c2d7f1c85
David <david.toledo@seoprohub.com>
2024-12-19 04:51:48 +0500

    adding

---
+++ types/generated/components.d.ts
@@ -0,0 +1,92 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    StructureDataMarkup: Schema.Attribute.Component<
+      'shared.structure-data-markup',
+      false
+    >;
+    TitleTag: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+export interface SharedStructureDataMarkup extends Struct.ComponentSchema {
+  collectionName: 'components_shared_structure_data_markups';
+  info: {
+    description: '';
+    displayName: 'Structure Data Markup';
+    icon: 'code';
+  };
+  attributes: {
+    code: Schema.Attribute.JSON;
+    Name: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+      'shared.structure-data-markup': SharedStructureDataMarkup;
+    }
+  }
+}
